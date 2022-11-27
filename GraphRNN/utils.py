b49456a6f2ecfb3213aa2cb5b1444559186b367f
vydu <vydu@unc.edu>
2022-11-27 18:35:49 -0500

    added ZINC generation functionality

--- GraphRNN/utils.py
+++ GraphRNN/utils.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.init as init
 import torch_geometric
-import rdkit, rdkit.Chem
+#import rdkit, rdkit.Chem
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
